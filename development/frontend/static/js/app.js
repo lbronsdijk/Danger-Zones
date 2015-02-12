53b9383a3d315757eb33311de6c580646bc53854
Luc Bronsdijk <info@lucbronsdijk.nl>
2015-02-12 12:54:59 +0100

    A href fix

--- development/frontend/static/js/app.js
+++ development/frontend/static/js/app.js
@@ -1,3 +1,15 @@
+// Standalone app fix
+var a = document.getElementsByTagName("a");
+
+for(var i=0;i<a.length;i++)
+{
+    a[i].onclick=function()
+    {
+        window.location=this.getAttribute("href");
+        return false
+    }
+}
+
 $(function() 
 {
     FastClick.attach(document.body);
